fe0e3cb38746557e6800c6c73318c1cabc734ab3
Trey Beason <trey.beason@yahoo.com>
2019-02-24 18:11:43 -0600

    Getters & Setters

--- src/src/Punch.java
+++ src/src/Punch.java
@@ -23,4 +23,86 @@ public class Punch {
         this.idNum = inBadge.getId();
         
     }
+
+    public int getIdNum() {
+        return idNum;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public int getmSecond() {
+        return mSecond;
+    }
+
+    public Badge getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setIdNum(int idNum) {
+        this.idNum = idNum;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public void setSecond(int second) {
+        this.second = second;
+    }
+
+    public void setmSecond(int mSecond) {
+        this.mSecond = mSecond;
+    }
+
+    public void setId(Badge id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
 }
