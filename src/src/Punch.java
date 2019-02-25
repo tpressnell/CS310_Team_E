f58f33b3d2838885d2ca06bfe0dbac884c83e018
Nic Morrison <nmorrison@stu.jsu.edu>
2019-02-24 18:29:03 -0600

    Made Calendar Obj

--- src/src/Punch.java
+++ src/src/Punch.java
@@ -1,6 +1,7 @@
 package src;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 import src.Badge;
 
 public class Punch {
@@ -8,6 +9,7 @@ public class Punch {
     private int idNum, year, month, day, hour, minute, second, mSecond;
     private Badge id;
     private String name;
+    private GregorianCalendar date;
     
     public Punch(Badge inBadge, int yr, int mth, int inDay, int hr, int min, int sec, int ms){
         this.id = inBadge;
@@ -23,4 +25,14 @@ public class Punch {
         this.idNum = inBadge.getId();
         
     }
+    // Make Greg Calendar Object 
+    
+    public GregorianCalendar makeCalndar(int calYear, int calMonth, int calDay, int calHour, int calMinute, int calSecond) {
+        
+        GregorianCalendar date = new GregorianCalendar(calYear, calMonth, calDay,calHour, calMinute, calSecond);
+        
+      return date;  
+    }
+    
+    
 }
