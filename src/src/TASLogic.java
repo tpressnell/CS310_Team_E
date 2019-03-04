a6b986897888d6417052e797404a2499fb2112f8
Tyler Pressnell <tyler.pressnell86@gmail.com>
2019-03-04 12:10:17 -0600

    Feature 4 Started

---
+++ src/src/TASLogic.java
@@ -0,0 +1,11 @@
+
+package src;
+
+import java.util.ArrayList;
+
+public class TASLogic {
+    
+    public static int calculateTotalMinutes(ArrayList<Punch> dailyPunchList, Shift shift){
+    return 0;
+    }
+}
