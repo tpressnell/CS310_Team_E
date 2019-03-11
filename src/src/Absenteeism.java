d9e899226da12ca499affe0a282a9f1bcb2e5bc5
Austin McDaniel <amcdaniel4@stu.jsu.edu>
2019-03-11 11:38:50 -0500

    Absenteeism Class 1.0

--- src/src/Absenteeism.java
+++ src/src/Absenteeism.java
@@ -4,6 +4,48 @@ package src;
 
 public class Absenteeism {
     
+    private String ID;
+    private long timeStamp;
+    private double percentage;
     
+    public Absenteeism(String ID, long TS, double percent){
+        this.ID = ID;
+        this.timeStamp = TS;
+        this.percentage =  percent;
+        
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public double getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(double percentage) {
+        this.percentage = percentage;
+    }
+    
+    public String toString(){
+        
+        StringBuilder output = new StringBuilder();
+        
+        output.append("#" + ID);
+        output.append(" (Pay Period Starting " + timeStamp + "): ");
+        output.append(percentage + "%");
+    }
     
 }
