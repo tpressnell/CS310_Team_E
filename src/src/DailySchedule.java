2c00b0d4352bf8ff1a0f14d52eb5c80da661fb37
Austin McDaniel <amcdaniel4@stu.jsu.edu>
2019-04-01 11:47:17 -0500

    Work on Daily Schedule Class

--- src/src/DailySchedule.java
+++ src/src/DailySchedule.java
@@ -11,4 +11,82 @@ public class DailySchedule {
     private int grace_period;
     private int dock;
     private int lunchDeduct;
+    
+    public DailySchedule(long start, long stop, long lunch_start, long lunch_stop, int interval, int grace_period, int dock, int lunchDeduct){
+        this.start = start;
+        this.stop = stop;
+        this.lunch_start = lunch_start;
+        this.lunch_stop = lunch_stop;
+        this.interval = interval;
+        this.grace_period = grace_period;
+        this.dock = dock;
+        this.lunchDeduct = lunchDeduct;
+        
+    }
+
+    public long getStart() {
+        return start;
+    }
+
+    public void setStart(long start) {
+        this.start = start;
+    }
+
+    public long getStop() {
+        return stop;
+    }
+
+    public void setStop(long stop) {
+        this.stop = stop;
+    }
+
+    public long getLunch_start() {
+        return lunch_start;
+    }
+
+    public void setLunch_start(long lunch_start) {
+        this.lunch_start = lunch_start;
+    }
+
+    public long getLunch_stop() {
+        return lunch_stop;
+    }
+
+    public void setLunch_stop(long lunch_stop) {
+        this.lunch_stop = lunch_stop;
+    }
+
+    public int getInterval() {
+        return interval;
+    }
+
+    public void setInterval(int interval) {
+        this.interval = interval;
+    }
+
+    public int getGrace_period() {
+        return grace_period;
+    }
+
+    public void setGrace_period(int grace_period) {
+        this.grace_period = grace_period;
+    }
+
+    public int getDock() {
+        return dock;
+    }
+
+    public void setDock(int dock) {
+        this.dock = dock;
+    }
+
+    public int getLunchDeduct() {
+        return lunchDeduct;
+    }
+
+    public void setLunchDeduct(int lunchDeduct) {
+        this.lunchDeduct = lunchDeduct;
+    }
+    
+    
 }
