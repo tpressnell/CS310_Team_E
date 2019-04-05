13ee9c5f267d8def0783ba791ea556bc6bfe3861
Austin McDaniel <amcdaniel4@stu.jsu.edu>
2019-04-05 12:11:27 -0500

    Added Accesors for workSchedule

--- src/src/Shift.java
+++ src/src/Shift.java
@@ -1,6 +1,8 @@
 package src;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.HashMap;
 
 public class Shift {
@@ -33,6 +35,20 @@ public class Shift {
     public DailySchedule getDailySchdedule(int dayOfWeek){
         return workSchedule.get(dayOfWeek);
     }
+    
+    public int getShiftStartHour(){
+        GregorianCalendar g = new GregorianCalendar();
+        g.setTimeInMillis(defaultSchedule.getStart());
+        return g.get(Calendar.HOUR_OF_DAY);
+    }
+    
+    public int getShiftStartHour(int day){
+        DailySchedule DS = workSchedule.get(day);
+        GregorianCalendar g = new GregorianCalendar();
+        g.setTimeInMillis(DS.getStart());
+        return g.get(Calendar.HOUR_OF_DAY);
+    }
+    
     public int getShiftLength() {
         long shiftLengthInMillis = 0;
         int shiftLengthInMinutes = 0;
@@ -42,6 +58,19 @@ public class Shift {
         shiftLengthInMinutes = (int) shiftLengthInMillis / CONVERSION_TO_MINUTES;
         
        return shiftLengthInMinutes; 
+    }
+    
+    public int getShiftLength(int day){
+        DailySchedule DS = workSchedule.get(day);
+        long shiftLengthInMillis = 0;
+        int shiftLengthInMinutes = 0;
+        final int CONVERSION_TO_MINUTES = 60000;
+        
+        shiftLengthInMillis = (DS.getStop() - DS.getStart());
+        shiftLengthInMinutes = (int) shiftLengthInMillis / CONVERSION_TO_MINUTES;
+        
+       return shiftLengthInMinutes; 
+        
     }
     
     public int getLunchLength(){
@@ -55,22 +84,55 @@ public class Shift {
        return lunchLengthInMinutes;
         
     }
+    
+    public int getLunchLength(int day){
+        DailySchedule DS = workSchedule.get(day);
+        long lunchLengthInMillis = 0;
+        int lunchLengthInMinutes = 0;
+        final int CONVERSION_TO_MINUTES = 60000;
+        
+        lunchLengthInMillis = (DS.getLunch_stop() - DS.getLunch_start());
+        lunchLengthInMinutes = (int) lunchLengthInMillis / CONVERSION_TO_MINUTES;
+        
+        return lunchLengthInMinutes;
+    }
 
     public long getStart_Time() {
         return defaultSchedule.getStart();
+    }
+    
+    public long getStart_Time(int day){
+        DailySchedule DS = workSchedule.get(day);
+        return DS.getStart();
     }
 
     public void setStart_Time(long start_time) {
         this.defaultSchedule.setStart(start_time);
     }
+    public void setStart_Time(long start_time, int day){
+        DailySchedule DS = workSchedule.get(day);
+        DS.setStart(start_time);
+        workSchedule.put(day, DS);
+    }
 
     public long getStop_Time() {
         return defaultSchedule.getStop();
+    }
+    
+    public long getStop_Time(int day){
+        DailySchedule DS = workSchedule.get(day);
+        return DS.getStop();    
     }
 
     public void setStop_Time(long Stop_time) {
         this.defaultSchedule.setStop(Stop_time);
         
+    }
+    
+    public void setStop_Time(long stop_time, int day){
+        DailySchedule DS = workSchedule.get(day);
+        DS.setStop(stop_time);
+        workSchedule.put(day, DS);
     }
     
     public int getID(){
@@ -85,16 +147,38 @@ public class Shift {
         return defaultSchedule.getLunch_start();
     }
     
+    public long getLunch_Start(int day){
+        DailySchedule DS = workSchedule.get(day);
+        return DS.getLunch_start();
+    }
+    
     public void setLunch_Start(long newLunch_start){
         this.defaultSchedule.setLunch_start(newLunch_start);
+    }
+    
+    public void setLunch_Start(long lunch_start, int day){
+        DailySchedule DS = workSchedule.get(day);
+        DS.setLunch_start(lunch_start);
+        workSchedule.put(day, DS);
     }
     
     public long getLunch_Stop(){
         return defaultSchedule.getLunch_stop();
     }
     
+    public long getLunch_Stop(int day){
+        DailySchedule DS = workSchedule.get(day);
+        return DS.getLunch_stop();
+    }
+    
     public void setLunch_Stop(long newLunch_stop){
         this.defaultSchedule.setLunch_stop(newLunch_stop);
+    }
+    
+    public void setLunch_Stop(long lunch_stop, int day){
+        DailySchedule DS = workSchedule.get(day);
+        DS.setLunch_stop(lunch_stop);
+        workSchedule.put(day, DS);
     }
     
     public int getNumOfDaysInShift(){
