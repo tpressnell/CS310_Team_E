252b756bc2a12625f6cf0ecb326e064ec2f6bce0
Tyler Pressnell <tyler.pressnell86@gmail.com>
2019-04-04 19:29:00 -0500

    Feature 8 ready for hashmap

--- src/src/Shift.java
+++ src/src/Shift.java
@@ -24,10 +24,22 @@ public class Shift {
         int shiftLengthInMinutes = 0;
         final int CONVERSION_TO_MINUTES = 60000;
         
-        shiftLengthInMillis = (this.defaultSchedule.getStop() - this.defaultSchedule.getLunch_start() - (this.defaultSchedule.getLunch_stop() - this.defaultSchedule.getLunch_start()));
+        shiftLengthInMillis = (this.defaultSchedule.getStop() - this.defaultSchedule.getStart());
         shiftLengthInMinutes = (int) shiftLengthInMillis / CONVERSION_TO_MINUTES;
         
        return shiftLengthInMinutes; 
+    }
+    
+    public int getLunchLength(){
+        long lunchLengthInMillis = 0;
+        int lunchLengthInMinutes = 0;
+        final int CONVERSION_TO_MINUTES = 60000;
+        
+        lunchLengthInMillis = (this.defaultSchedule.getLunch_stop() - this.defaultSchedule.getLunch_start());
+        lunchLengthInMinutes = (int) lunchLengthInMillis / CONVERSION_TO_MINUTES;
+        
+       return lunchLengthInMinutes;
+        
     }
 
     public long getStart_Time() {
@@ -84,12 +96,10 @@ public class Shift {
         
         StringBuilder output = new StringBuilder();
         
-        String start = new SimpleDateFormat("HH:mm:ss").format(defaultSchedule.getStart());
-        String stop = new SimpleDateFormat("HH:mm:ss").format(defaultSchedule.getStop());
-        String lunch_start = new SimpleDateFormat("HH:mm:ss").format(defaultSchedule.getLunch_start());
-        String lunch_stop = new SimpleDateFormat("HH:mm:ss").format(defaultSchedule.getLunch_stop());
-        long lunch_length = defaultSchedule.getLunch_stop() - defaultSchedule.getLunch_start() / 60000;
-        String lunch_length_string = new SimpleDateFormat("HH:mm:ss").format(lunch_length);
+        String start = new SimpleDateFormat("HH:mm").format(defaultSchedule.getStart());
+        String stop = new SimpleDateFormat("HH:mm").format(defaultSchedule.getStop());
+        String lunch_start = new SimpleDateFormat("HH:mm").format(defaultSchedule.getLunch_start());
+        String lunch_stop = new SimpleDateFormat("HH:mm").format(defaultSchedule.getLunch_stop());
         
         output.append(this.description + ": ");
         output.append(start + " - ");
@@ -97,7 +107,7 @@ public class Shift {
         output.append(this.getShiftLength() + " minutes); Lunch: ");
         output.append(lunch_start + " - ");
         output.append(lunch_stop + " (");
-        output.append(lunch_length_string + " minutes)");        
+        output.append(this.getLunchLength() + " minutes)");        
         
         return output.toString();
     }
