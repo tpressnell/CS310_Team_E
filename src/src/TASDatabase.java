a475c05cdb2114a54fcc4d0a6a7f53cb0f42b4d4
Tyler Pressnell <tyler.pressnell86@gmail.com>
2019-04-11 19:50:00 -0500

    what is happening

--- src/src/TASDatabase.java
+++ src/src/TASDatabase.java
@@ -276,7 +276,6 @@ public class TASDatabase {
             int lunchDeduct = resultset.getInt(9);
             
             DailySchedule dailySchedule = new DailySchedule(start, stop, lunch_start, lunch_stop, interval, grace_period, dock, lunchDeduct);
-            Timestamp Jenny = new Timestamp(garry.getTimeInMillis());
             
             Shift newShift = new Shift(dailyScheduleId, description, dailySchedule);
                 
@@ -292,48 +291,51 @@ public class TASDatabase {
             resultset = pstSelect.getResultSet();
             resultset.first();
             
-            if( resultset.next()) {
+            while(resultset.next()) {
                 
-                String originalTimestamp = new SimpleDateFormat("yyyy-MM-dd").format(ts);
+                String originalTimestamp = new SimpleDateFormat("yyyy-MM-dd").format(garry.getTimeInMillis());
                 int day = resultset.getInt(5);
                 int newDailyScheduleId = resultset.getInt(6);
                 
-                if(resultset.getString(2).contains(originalTimestamp)){
-                    query = "SELECT * FROM dailyschedule WHERE id = ?";
-                    pstSelect = conn.prepareStatement(query);
-                    pstSelect.setInt(1, newDailyScheduleId);
-                    pstSelect.execute();
-                    resultset = pstSelect.getResultSet();
-                    resultset.first();
+                if(b.getId().contains(resultset.getString(4)) || resultset.getString(4) == null){
+                    if(resultset.getString(2).contains(originalTimestamp)){
+                        query = "SELECT * FROM dailyschedule WHERE id = ?";
+                        pstSelect = conn.prepareStatement(query);
+                        pstSelect.setInt(1, newDailyScheduleId);
+                        pstSelect.execute();
+                        resultset = pstSelect.getResultSet();
+                        resultset.first();
 
-                    nts = resultset.getTimestamp(2);
-                    long newStart = nts.getTime();
+                        nts = resultset.getTimestamp(2);
+                        long newStart = nts.getTime();
 
-                    nts = resultset.getTimestamp(3);
-                     long newStop = nts.getTime();
+                        nts = resultset.getTimestamp(3);
+                        long newStop = nts.getTime();
 
-                    int newInterval = resultset.getInt(4);
-                    int newGrace_period = resultset.getInt(5);
-                    int newDock = resultset.getInt(6);
-
-
-                    nts = resultset.getTimestamp(7);
-                    long newLunch_start = nts.getTime();
-
-                    nts = resultset.getTimestamp(8);
-                    long newLunch_stop = nts.getTime();
-                    int newLunchDeduct = resultset.getInt(9);
-
-                    DailySchedule overrideDailySchedule = new DailySchedule(newStart, newStop, newLunch_start, newLunch_stop, newInterval, newGrace_period, newDock, newLunchDeduct);
+                        int newInterval = resultset.getInt(4);
+                        int newGrace_period = resultset.getInt(5);
+                        int newDock = resultset.getInt(6);
 
 
+                        nts = resultset.getTimestamp(7);
+                        long newLunch_start = nts.getTime();
+
+                        nts = resultset.getTimestamp(8);
+                        long newLunch_stop = nts.getTime();
+                        int newLunchDeduct = resultset.getInt(9);
+
+                        DailySchedule overrideDailySchedule = new DailySchedule(newStart, newStop, newLunch_start, newLunch_stop, newInterval, newGrace_period, newDock, newLunchDeduct);
 
 
-                    newShift.setOverride(overrideDailySchedule, day);
 
 
-                    return newShift;
-                } 
+                        newShift.setOverride(overrideDailySchedule, day);
+
+
+                        return newShift;
+                    } 
+            
+                }
             }
             
             
